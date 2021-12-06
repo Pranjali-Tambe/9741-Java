82f270fa3f948a870065ae396ff59fa4cb96779f
Pranjali-Tambe <86174318+Pranjali-Tambe@users.noreply.github.com>
2021-12-06 21:22:44 +0530

    Add files via upload

---
+++ AnudipPractical/ObjectClass.java
@@ -0,0 +1,10 @@
+class ObjectClass{
+int id;
+String name;
+public static void main(String args[]){
+
+ObjectClass s1=new ObjectClass();
+System.out.println(s1.id);
+System.out.println(s1.name);
+
+}}
