19d674e1efcf6ce9c4b67287ea87db962f09c438
Pranjali-Tambe <86174318+Pranjali-Tambe@users.noreply.github.com>
2021-12-06 21:23:20 +0530

    Add files via upload

---
+++ AnudipPractical/Object.java
@@ -0,0 +1,12 @@
+class Object{
+String Obbook;
+int Obprice;
+Object(String book,int price){
+this.Obbook=book;
+this.Obprice=price;}
+public static void main(String args[]){
+Object ob=new Object("English", 100);
+System.out.println(ob.Obbook+""ob.Obprice);
+
+
+}}
