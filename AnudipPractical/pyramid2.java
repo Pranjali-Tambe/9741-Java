92def6a31809ba4d7986f48ee8c507b8512aa22f
Pranjali-Tambe <86174318+Pranjali-Tambe@users.noreply.github.com>
2021-12-06 21:28:38 +0530

    Update pyramid2.java

--- AnudipPractical/pyramid2.java
+++ AnudipPractical/pyramid2.java
@@ -1,8 +1,14 @@
-public class pyramid2{
-public static void main(String args[]){
-for(int i=0;i<=6;i++){
-for(int j=6;j>=i;j--){
+public class pyramid2
+{
+public static void main(String args[])
+{
+for(int i=0;i<=6;i++)
+{
+for(int j=6;j>=i;j--)
+{
 System.out.print("*");
 }
 System.out.println();
-}}}
+}
+}
+}
