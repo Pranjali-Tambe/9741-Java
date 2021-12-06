5b2ca8bb4f9fc824ed2416e0e45ded7dea0d4f34
Pranjali-Tambe <86174318+Pranjali-Tambe@users.noreply.github.com>
2021-12-06 21:26:41 +0530

    Add files via upload

---
+++ AnudipPractical/RealtionalOperator.java
@@ -0,0 +1,19 @@
+public class RealtionalOperator{
+public static void main(String args[]){
+int num1=10;
+int num2=20;
+if(num2==num1)
+{
+System.out.println("num1 and num2 are not equal ");
+}
+else{
+System.out.println("num1 and num2 are equal ");
+}
+if(num2!=num1)
+{
+System.out.println("num1 and num2 are not equal ");
+}
+else{
+System.out.println("num1 and num2 are equal ");
+}
+}}
