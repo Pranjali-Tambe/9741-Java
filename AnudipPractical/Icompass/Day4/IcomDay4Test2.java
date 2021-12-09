18eace4d3479d5e25564e98bb694038f18d8e777
Pranjali-Tambe <86174318+Pranjali-Tambe@users.noreply.github.com>
2021-12-09 21:41:50 +0530

    Add files via upload

---
+++ AnudipPractical/Icompass/Day4/IcomDay4Test2.java
@@ -0,0 +1,25 @@
+class A
+{
+final void m1()
+{
+System.out.println("m1 called");
+}
+}
+class B extends A
+{
+public void m2()
+{
+m1();
+System.out.println("m2 called");
+}
+}
+
+public class IcomDay4Test2
+{
+public static void main(String args[])
+{
+B b=new B();
+b.m1();
+b.m2();
+}
+}
