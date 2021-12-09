bd024d5df0a5b6e706f64a6c11e07727d5421541
Pranjali-Tambe <86174318+Pranjali-Tambe@users.noreply.github.com>
2021-12-09 21:02:28 +0530

    Add files via upload

---
+++ AnudipPractical/Icompass/day3/IcomDay3Test2.java
@@ -0,0 +1,15 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+class IcomDay3Test2
+{
+private static String regex="dog";
+private static String input="The dog says meow."+"All dogs say meow.";
+private static String replace="cat";
+public static void main(String args[])
+{
+Pattern p=Pattern.compile(regex);
+Matcher m=p.matcher(input);
+input=m.replaceAll(replace);
+System.out.println(input);
+}
+}
